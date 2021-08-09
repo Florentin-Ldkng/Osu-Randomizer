d3fea366e95d8a64bbaff2129a44c45e4b014394
Florentin <florentin.luedeking@t-online.de>
2021-08-09 23:55:01 +0200

    Added Credentials Class

---
+++ OsuRandomizer/OsuRandomizer/Credentials.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OsuRandomizer
+{
+    static class Credentials
+    {
+        private static string discordToken = "";
+        private static string dbConnectionString = "";
+    }
+}
